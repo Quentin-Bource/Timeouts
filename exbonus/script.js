f26b54da90eabd7046309a652e3758c26342c7b8
Quentin <quentin01.bource@gmail.com>
2022-12-13 21:36:36 +0100

    taupe

--- exbonus/script.js
+++ exbonus/script.js
@@ -2,8 +2,8 @@ let bgcolor = document.querySelectorAll(".trou")
 
 function switchback() {
     let switchbgcolor = Math.floor(Math.random() * bgcolor.length);
-
     bgcolor[switchbgcolor].style.background = "red"
+
 }
 
 switchback()
